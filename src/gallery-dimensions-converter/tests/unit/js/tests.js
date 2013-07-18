01c4e68b0d4c05339c8b367c75fd518b78df8569
kenjiru <kenjiru.ro@gmail.com>
2013-07-18 17:25:26 +0200

    Added proper tests.

--- src/gallery-dimensions-converter/tests/unit/js/tests.js
+++ src/gallery-dimensions-converter/tests/unit/js/tests.js
@@ -4,22 +4,34 @@ YUI.add('module-tests', function(Y) {
 
     suite.add(new Y.Test.Case({
         name: 'Automated Tests',
+        l : null, // the instance used for tests
 
-        'test is empty': function() {
+        'create instance': function() {
             // TODO Create proper tests
-            var l = new Dimension('20.4cm');
+            this.l = new Y.Dimension('20.4cm');
 
-            console.log("l.toString(): 20.4cm: " + l);
-            console.log("l.convertTo('mm'): 204: " + l.convertTo('mm'));
+            Y.Assert.isNotNull(this.l);
+        },
 
-            console.log("l.add('20mm'): 22.4cm: " + l.add('20mm'));
-            console.log("l.add(['20mm', '1.5cm', '1in']): : " + l.add(['20mm', '1.5cm', '1in']));
+        'toString' : function() {
+            Y.Assert.areEqual(this.l.toString(), '20.4cm', 'toString returned a different value!');
+        },
 
-            console.log("l.subtract('10mm'): 21.4cm: " + l.subtract('10mm'));
-            console.log("l.subtract(['5mm', '0.3in', '10px']): : " + l.subtract(['5mm', '0.3in', '10px']));
+        'convertTo' : function() {
+            Y.Assert.areEqual(this.l.convertTo('mm'), '204.6771784232365mm', 'convertTo returned a different value!');
+        },
+
+        'addition methods' : function() {
+            Y.Assert.areEqual(this.l.add('20mm'), '22.393382961124896cm', "add('20mm') returned a different value!");
+            Y.Assert.areEqual(this.l.add(['20mm', '1.5cm', '1in']), '28.427708850289495cm', "add(['20mm', '1.5cm', '1in']) returned a different value!");
+        },
+
+        'subtraction methods' : function() {
+            Y.Assert.areEqual(this.l.subtract('10mm'), '27.431017369727044cm', "subtract('10mm') returned a different value!");
+            Y.Assert.areEqual(this.l.subtract(['5mm', '0.3in', '10px']), '25.905707196029777cm', "subtract(['5mm', '0.3in', '10px']) returned a different value!");
         }
     }));
 
     Y.Test.Runner.add(suite);
 
-},'', { requires: [ 'test' ] });
+},'', { requires: [ 'test', 'gallery-dimensions-converter' ] });
